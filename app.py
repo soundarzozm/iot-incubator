c8047e34a39ec19f32eb13c524498bf077695a63
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-06-01 21:07:47 +0530

    working code

---
+++ app.py
@@ -0,0 +1,44 @@
+import conf
+from boltiot import Sms, Bolt
+import json, time
+
+minimum_limit = 300
+maximum_limit = 600
+threshold = 380
+
+def toFahrenheit(temp):
+    fahrenheit = ((temp/10) * 1.8) + 32
+    return int(fahrenheit)
+
+mybolt = Bolt(conf.API_KEY, conf.DEVICE_ID)
+sms = Sms(conf.SID, conf.AUTH_TOKEN, conf.TO_NUMBER, conf.FROM_NUMBER)
+
+print("Running")
+status = mybolt.isOnline()
+print(status)
+
+if status[-2]=="1":
+    mybolt.digitalWrite("4", "HIGH")
+else:
+    mybolt.digitalWrite("4", "LOW")
+    print("An error has occured.")
+    exit()
+
+while True:
+    try:
+        response = mybolt.analogRead("A0")
+        data = json.loads(response)
+        data = int(data["value"])
+        print("Sensor value:", str(data))
+        if data > threshold:
+            sms.send_sms("PLEASE CONTACT THE HOSPITAL AUTHORITIES! The current temperature of the incubator is " + str(toFahrenheit(data))+"°F.")
+            mybolt.digitalWrite("1", "HIGH")
+            mybolt.digitalWrite("2", "HIGH")
+        else:
+            mybolt.digitalWrite("1", "LOW")
+            mybolt.digitalWrite("2", "LOW")
+        time.sleep(5)
+    except KeyboardInterrupt:
+        mybolt.digitalWrite("4", "LOW")
+        print("Interrupted")
+        exit()
