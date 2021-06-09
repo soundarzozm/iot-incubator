da8ad713a1ceb29529e61a0b74c668f195954082
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-06-09 22:53:54 +0530

    code final

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ threshold = 380
 
 def toFahrenheit(temp):
     fahrenheit = ((temp/10) * 1.8) + 32
-    return int(fahrenheit) 
+    return int(fahrenheit)
 
 mybolt = Bolt(conf.API_KEY, conf.DEVICE_ID)
 sms = Sms(conf.SID, conf.AUTH_TOKEN, conf.TO_NUMBER, conf.FROM_NUMBER)
@@ -28,20 +28,20 @@ while True:
         response = mybolt.analogRead("A0")
         data = json.loads(response)
         data = int(data["value"])
-        
-        print("Sensor value:", str(data))
-        
+
+        print("Sensor value:", str(toFahrenheit(data))+"°F.")
+
         if data > threshold:
             sms.send_sms("PLEASE CONTACT THE HOSPITAL AUTHORITIES! The current temperature of the incubator is " + str(toFahrenheit(data))+"°F.")
             mybolt.digitalWrite("1", "HIGH")
             mybolt.digitalWrite("2", "HIGH")
-        
+
         else:
             mybolt.digitalWrite("1", "LOW")
             mybolt.digitalWrite("2", "LOW")
-        
+
         time.sleep(5)
-    
+
     except KeyboardInterrupt:
         mybolt.digitalWrite("4", "LOW")
         print("Interrupted")
